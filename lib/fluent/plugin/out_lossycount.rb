62644f766cb1182dfce8c9f13fea73d3833c6f05
Takeshi Koya <koya-t@asahi.com>
2020-11-26 15:24:40 +0900

    add modifications to migrate to fluentd v1.0

--- lib/fluent/plugin/out_lossycount.rb
+++ lib/fluent/plugin/out_lossycount.rb
@@ -1,4 +1,9 @@
-class Fluent::LossyCountOutput < Fluent::Output
+require 'fluent/plugin/output'
+
+module Fluent::Plugin
+  class LossyCountOutput < Output
+    helpers :compat_parameters, :event_emitter
+
     Fluent::Plugin.register_output('lossycount', self)
 
     config_param :key_name, :string, :default => nil
@@ -14,182 +19,180 @@ class Fluent::LossyCountOutput < Fluent::Output
     config_param :verbose, :bool, :default => false
 
     def configure(config)
-        super
-        unless config.has_key?('key_name')
-            raise Fluent::ConfigError, "you must set 'key_name'"
-        end
-        unless config.has_key?('output_tag')
-            raise Fluent::ConfigError, "you must set 'output_tag'"
-        end
+      super
+      unless config.has_key?('key_name')
+        raise Fluent::ConfigError, "you must set 'key_name'"
+      end
+      unless config.has_key?('output_tag')
+        raise Fluent::ConfigError, "you must set 'output_tag'"
+      end
 
-        @mutex = Mutex.new
-        @sleep_wait = 0.4
-        init_counter()
+      @mutex = Mutex.new
+      @sleep_wait = 0.4
+      init_counter()
     end
 
     def start
-        super
-        start_watch
+      super
+      start_watch
     end
 
     def shutdown
-        super
-        @watcher.terminate
-        @watcher.join
+      @watcher.terminate
+      @watcher.join
+      super
     end
 
     def init_counter()
-        @counter = Counter::LossyCounter.new({:gamma => @gamma, :epsilon => @epsilon})
+      @counter = Counter::LossyCounter.new({:gamma => @gamma, :epsilon => @epsilon})
     end
 
     def flush_counter(step)
-        freq_counter = {}
-        metrics = {}
-        @mutex.synchronize {
-            freq_counter = @counter.get()
-            if @enable_metrics
-                metrics = @counter.get_metrics()
-            end 
-            init_counter()
-        }
-        flush_time = Fluent::Engine.now.to_i
-        if @verbose
-            $log.info "flushtime : " + flush_time.to_s
-            $log.info "{ "
-        end
-        freq_counter.each_pair { |key, value|
-            map = {@output_key_name => key, @output_timestamp_name => flush_time, @output_value_name => value}
-            if @verbose
-                $log.info map
-            end
-            Fluent::Engine.emit(@output_tag, Fluent::Engine.now, map)
-        }
-        if @verbose
-            $log.info "}"
-        end
-
+      freq_counter = {}
+      metrics = {}
+      @mutex.synchronize {
+        freq_counter = @counter.get()
         if @enable_metrics
-            if @verbose
-                $log.info "metrics : " + metrics.to_s
-            end
-            Fluent::Engine.emit(@metrics_tag, Fluent::Engine.now, metrics)
+          metrics = @counter.get_metrics()
+        end 
+        init_counter()
+      }
+      flush_time = Fluent::Engine.now.to_i
+      if @verbose
+        $log.info "flushtime : " + flush_time.to_s
+        $log.info "{ "
+      end
+      freq_counter.each_pair { |key, value|
+        map = {@output_key_name => key, @output_timestamp_name => flush_time, @output_value_name => value}
+        if @verbose
+          $log.info map
         end
+        router.emit(@output_tag, Fluent::Engine.now, map)
+      }
+      if @verbose
+        $log.info "}"
+      end
+
+      if @enable_metrics
+        if @verbose
+          $log.info "metrics : " + metrics.to_s
+        end
+        router.emit(@metrics_tag, Fluent::Engine.now, metrics)
+      end
     end
 
     def start_watch
-        @watcher = Thread.new{
-            @last_checked = Fluent::Engine.now.to_i
-            while true
-                sleep @sleep_wait
-                now = Fluent::Engine.now.to_i
-                if now - @last_checked >= @time_windows
-                    flush_counter(now - @last_checked)
-                    @last_checked = now
-                end
-            end
-        }
+      @watcher = Thread.new{
+        @last_checked = Fluent::Engine.now.to_i
+        while true
+          sleep @sleep_wait
+          now = Fluent::Engine.now.to_i
+          if now - @last_checked >= @time_windows
+            flush_counter(now - @last_checked)
+            @last_checked = now
+          end
+        end
+      }
     end
 
-    def emit(tag, es, chain)
-        es.each {|time,record|
-            k = traverse(record, @key_name)
-            if k
-                if k.is_a?(Array)
-                    k.each{ |v|
-                        @counter.add(v.to_s)
-                    }
-                else
-                    @counter.add(k.to_s)
-                end
-            end
-        }
-        chain.next
+    def process(tag, es)
+      es.each {|time,record|
+        k = traverse(record, @key_name)
+        if k
+          if k.is_a?(Array)
+            k.each{ |v|
+              @counter.add(v.to_s)
+            }
+          else
+            @counter.add(k.to_s)
+          end
+        end
+      }
     end
 
     def traverse(data, key)
-        val = data
-        key.split('.').each{ |k|
-            if val.has_key?(k)
-                val = val[k]
-            else
-                return nil
-            end
-        }
-        return val
+      val = data
+      key.split('.').each{ |k|
+        if val.has_key?(k)
+          val = val[k]
+        else
+          return nil
+        end
+      }
+      return val
     end
+  end
 end
-
 
 module Counter
-    class LossyCounter
-        def initialize(config)
-            @gamma = config.has_key?(:gamma) ? config[:gamma].to_f : 0.005
-            @epsilon = config.has_key?(:epsilon) ? config[:epsilon].to_f : 0.001
-            @current = 1
-            @freq_counter = {}
-            @delta_counter = {}
-            @num = 0
-            @max_size = -1
-        end
-
-        def add(key)
-            if @freq_counter.has_key?(key)
-                @freq_counter[key] += 1
-            else
-                @freq_counter[key] = 1
-                @delta_counter[key] = @current - 1
-            end
-            @num += 1
-            if @num % (1 / @epsilon).to_i == 0
-                sweep()
-            end
-        end
-
-        def sweep()
-            length = @freq_counter.length
-            if @max_size < length
-                @max_size = length
-            end
-
-            @freq_counter.each_pair { |key, value|
-                if value <= (@current - @delta_counter[key])
-                    @freq_counter.delete(key)
-                    @delta_counter.delete(key)
-                end
-            }
-            @current += 1
-        end
-
-        def get()
-            buf = {}
-            @freq_counter.each_pair { |key, value|
-                if value > (@num * (@gamma - @epsilon) ).to_i
-                    buf[key] = value
-                end
-            }
-            return buf
-        end
-        
-        def get_num()
-            return @num
-        end 
-
-        def get_current_max_size()
-            return @max_size
-        end
-
-        def get_num_x_gamma()
-            return @num.to_f * @gamma
-        end
-
-        def get_num_x_gamma_d_epsilon()
-            return @num.to_f * (@gamma - @epsilon)
-        end
-
-        def get_metrics()
-            return {'num' => get_num(), 'max_size' => get_current_max_size(), 'current_size' => @freq_counter.size() , 'reduced_size' => get().size(), "gamma" => @gamma , "epsilon" => @epsilon , "n_x_gamma" => get_num_x_gamma(), "n_x_gamma-epsilon" => get_num_x_gamma_d_epsilon() }
-        end
-
+  class LossyCounter
+    def initialize(config)
+      @gamma = config.has_key?(:gamma) ? config[:gamma].to_f : 0.005
+      @epsilon = config.has_key?(:epsilon) ? config[:epsilon].to_f : 0.001
+      @current = 1
+      @freq_counter = {}
+      @delta_counter = {}
+      @num = 0
+      @max_size = -1
     end
-end
 
+    def add(key)
+      if @freq_counter.has_key?(key)
+        @freq_counter[key] += 1
+      else
+        @freq_counter[key] = 1
+        @delta_counter[key] = @current - 1
+      end
+      @num += 1
+      if @num % (1 / @epsilon).to_i == 0
+        sweep()
+      end
+    end
+
+    def sweep()
+      length = @freq_counter.length
+      if @max_size < length
+        @max_size = length
+      end
+
+      @freq_counter.each_pair { |key, value|
+        if value <= (@current - @delta_counter[key])
+          @freq_counter.delete(key)
+          @delta_counter.delete(key)
+        end
+      }
+      @current += 1
+    end
+
+    def get()
+      buf = {}
+      @freq_counter.each_pair { |key, value|
+        if value > (@num * (@gamma - @epsilon) ).to_i
+          buf[key] = value
+        end
+      }
+      return buf
+    end
+
+    def get_num()
+      return @num
+    end
+
+    def get_current_max_size()
+      return @max_size
+    end
+
+    def get_num_x_gamma()
+      return @num.to_f * @gamma
+    end
+
+    def get_num_x_gamma_d_epsilon()
+      return @num.to_f * (@gamma - @epsilon)
+    end
+
+    def get_metrics()
+      return {'num' => get_num(), 'max_size' => get_current_max_size(), 'current_size' => @freq_counter.size() , 'reduced_size' => get().size(), "gamma" => @gamma , "epsilon" => @epsilon , "n_x_gamma" => get_num_x_gamma(), "n_x_gamma-epsilon" => get_num_x_gamma_d_epsilon() }
+    end
+
+  end
+end
--- test/counter/test_lossy_counter.rb
+++ test/counter/test_lossy_counter.rb
@@ -1,73 +1,73 @@
 require 'helper'
 
 class LossyCounterTest < Test::Unit::TestCase
-    def setup
-        @gamma   = 0.005
-        @epsilon = 0.0045
-    end
+  def setup
+    @gamma = 0.005
+    @epsilon = 0.0045
+  end
 
-    def test_add_naive()
-        counter = Counter::LossyCounter.new({:gamma => @gamma, :epsilon => @epsilon})
-        counter.add('a')
-        counter.add('b')
-        counter.add('c')
-        counter.add('b')
-        counter.add('a')
-        counter.add('b')
+  def test_add_naive()
+    counter = Counter::LossyCounter.new({:gamma => @gamma, :epsilon => @epsilon})
+    counter.add('a')
+    counter.add('b')
+    counter.add('c')
+    counter.add('b')
+    counter.add('a')
+    counter.add('b')
 
-        freq_counter = counter.get()
+    freq_counter = counter.get()
 
-        p freq_counter
-        assert_equal freq_counter.has_key?('a') , true
-        assert_equal freq_counter.has_key?('b') , true
-        assert_equal freq_counter.has_key?('c') , true
-        assert_equal freq_counter.has_key?('d') , false
-        assert_equal freq_counter['a'] , 2
-        assert_equal freq_counter['b'] , 3
-        assert_equal freq_counter['c'] , 1
-        assert_equal freq_counter['d'] , nil
+    p freq_counter
+    assert_equal freq_counter.has_key?('a'), true
+    assert_equal freq_counter.has_key?('b'), true
+    assert_equal freq_counter.has_key?('c'), true
+    assert_equal freq_counter.has_key?('d'), false
+    assert_equal freq_counter['a'], 2
+    assert_equal freq_counter['b'], 3
+    assert_equal freq_counter['c'], 1
+    assert_equal freq_counter['d'], nil
 
-        p counter.get_metrics()
-    end
+    p counter.get_metrics()
+  end
 
-    def test_add_and_sweep()
-        counter = Counter::LossyCounter.new({:gamma => @gamma, :epsilon => @epsilon})
-        buf = {}
-        radix = [10000000, 1000000, 100000, 10000, 1000, 100, 10]
-        l = 100000
+  def test_add_and_sweep()
+    counter = Counter::LossyCounter.new({:gamma => @gamma, :epsilon => @epsilon})
+    buf = {}
+    radix = [10000000, 1000000, 100000, 10000, 1000, 100, 10]
+    l = 100000
 
-        (0..(l - 1)).step(1) do |i|
-            now = ((Time.now.to_f * 10000000) % 10000000).to_i
-            for r in radix
-                key = now & r
-                counter.add(key.to_s)
-                if buf.has_key?(key.to_s)
-                    buf[key] += 1
-                else
-                    buf[key] = 1
-                end
-            end
+    (0..(l - 1)).step(1) do |i|
+      now = ((Time.now.to_f * 10000000) % 10000000).to_i
+      for r in radix
+        key = now & r
+        counter.add(key.to_s)
+        if buf.has_key?(key.to_s)
+          buf[key] += 1
+        else
+          buf[key] = 1
         end
-
-        n = counter.get_num()
-        g = counter.get_num_x_gamma().to_i
-        e = (n * @epsilon).to_i
-        ge = counter.get_num_x_gamma_d_epsilon().to_i
-        freq_count = counter.get()
-
-        p counter.get_metrics()
-
-        buf.each_pair { |key, value|
-            if freq_count.has_key?(key)
-                if freq_count[key] >= g.to_i
-                    assert_equal value , freq_count[key]
-                else
-                    assert_operator e.to_i , :>= , (freq_count[key] - value).abs
-                end
-            else 
-                assert_operator e.to_i , :>= , value
-            end
-        }
-
+      end
     end
+
+    n = counter.get_num()
+    g = counter.get_num_x_gamma().to_i
+    e = (n * @epsilon).to_i
+    ge = counter.get_num_x_gamma_d_epsilon().to_i
+    freq_count = counter.get()
+
+    p counter.get_metrics()
+
+    buf.each_pair { |key, value|
+      if freq_count.has_key?(key)
+        if freq_count[key] >= g.to_i
+          assert_equal value, freq_count[key]
+        else
+          assert_operator e.to_i, :>=, (freq_count[key] - value).abs
+        end
+      else
+        assert_operator e.to_i, :>=, value
+      end
+    }
+
+  end
 end
